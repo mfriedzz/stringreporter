f5262dd0348db7c32fc59fe8a498aa157200fd2c
Michael F <mfriedmanzz01@gmail.com>
2015-01-19 12:25:15 -0700

    js string

---
+++ stringreporter.js
@@ -0,0 +1,18 @@
+
+
+var word1 = prompt("what is the word?");
+var word2 = prompt("what is the word? ");
+
+// if (word1 === null) or (word2 === null) {
+// 	console.log("Please enter a word");
+// 	break;
+// } 
+
+
+
+console.log(word1 + word2);
+var countofwords1 = word1.length;
+var countofwords2 = word2.length;
+
+console.log("Here is word 1 " + word1 + " number of characters " + countofwords1);
+console.log("Here is word 2 " + word2 + " number of characters " + countofwords2);
